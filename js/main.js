cdb0816331ebfe01b67129190031a9b202bff251
Artem Bogomolov <artem.bogomolov@segmentstream.com>
2019-08-07 00:08:26 +0300

    added search field

--- js/main.js
+++ js/main.js
@@ -70,4 +70,116 @@ $(document).ready(function() {
         }
     });
 
+    $('#clearIcon').click(function() {
+      $('#searchForm').removeClass('extended');
+      $('#searchField').val('');
+      $('#searchField').focus();
+      $('.searchHints').hide();
+    });
+
+    loadSearch();
+
 })
+
+function loadSearch(){
+  idx = lunr(function(){
+    this.field('url');
+    this.field('title', { boost: 10 });
+    this.field('summary');
+  })
+
+  $.getJSON('/content.json', function(data){
+    window.searchData = data;
+    $.each(data, function(index, entry){
+      idx.add($.extend({"id": index}, entry))
+    })
+  })
+
+  $('#search').on('click', function(){
+    $('.searchForm').toggleClass('show');
+    $('.searchHints').hide();
+  });
+
+  $('#search-toggle').on('click', function(){
+    $('.searchForm').toggleClass('show');
+    $('#searchField').focus();
+  });
+
+  $('#searchForm input').blur(function(){
+    if ($('.searchHints').is(":hover")) {
+    } else {
+      $('.searchHints').hide();
+      $('#searchForm').removeClass('extended');
+    }
+  });
+
+  $('#leftIcon').on('click', function(){
+    $('.searchForm').toggleClass('show');
+  });
+
+  $('#searchForm').on('keyup', function(e){
+    var code = (e.keyCode ? e.keyCode : e.which);
+    results = idx.search($('#searchField').val());
+    $('.searchHints').html('');
+    $('.searchHints').append('<ul class="searchResults"></ul>');
+    if (results.length > 0) {
+      $.each(results, function(index, result){
+        entry = window.searchData[result.ref];
+        $('.searchHints').show();
+        $('#searchForm').addClass('extended');
+        $('.searchResults').append('<li><a href="' + entry.url + '">' + entry.title + '</li>')
+      })
+    } else {
+      $('.searchHints').hide();
+      $('#searchForm').removeClass('extended');
+    }
+    if (code == 13) {
+      $('#searchForm').removeClass('extended');
+      $('.searchHints').hide();
+    }
+
+  });
+
+  $('#searchForm').on('submit', function(e){
+    e.preventDefault()
+    results = idx.search($('#searchField').val())
+    $('#content').html('<h1 class="searchHeader">Search results</h1>')
+    $('#content').append(
+        '<div class="bodySearchResults">\
+            <div class="resultCat forAnalystResults">Analyst<ul></ul></div>\
+            <div class="resultCat forDeveloperResults">Developer<ul></ul></div>\
+            <div class="resultCat ecommerceResults">Ecommerce<ul></ul></div>\
+            <div class="resultCat digitalDataResults">Variable list<ul></ul></div>\
+            <div class="resultCat eventResults">Events list<ul></ul></div>\
+            <div class="resultCat integrationResults">Integration list<ul></ul></div>\
+            <div class="resultCat dataSourcesResults">Automatic Data Import<ul></ul></div>\
+        </div>'
+    )
+
+    if(!results.length) {
+      $('#content').append('<div class="noSearchResults">No results found.</div>');
+    }
+
+    $.each(results, function(index, result){
+      entry = window.searchData[result.ref]
+      var resultType;
+      if (entry.url.indexOf('/events/') > -1) {
+          resultType = 'eventResults';
+      } else if (entry.url.indexOf('/digitaldata/') > -1) {
+          resultType = 'digitalDataResults';
+      } else if (entry.url.indexOf('/ecommerce/') > -1) {
+          resultType = 'ecommerceResults';
+      } else if (entry.url.indexOf('/for-analyst/') > -1) {
+          resultType = 'forAnalystResults';
+      } else if (entry.url.indexOf('/for-developer/') > -1) {
+          resultType = 'forDeveloperResults';
+      } else if (entry.url.indexOf('/integrations/') > -1) {
+          resultType = 'integrationResults';
+      } else if (entry.url.indexOf('/datasources/') > -1) {
+          resultType = 'dataSourcesResults';
+      }
+      $('.' + resultType).show();
+      $('.searchResults, .bodySearchResults .' + resultType + ' ul').append('<li><a href="' + entry.url + '">' + entry.title + '</li>')
+    })
+  })
+}
